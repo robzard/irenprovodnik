5054cf6aa35db68def04c075893147dec240d668
robzard <pal-vitek1111@mail.ru>
2024-05-02 18:07:11 +0500

    payment

--- app/utils/telegram_bot.py
+++ app/utils/telegram_bot.py
@@ -5,7 +5,13 @@ from aiogram import Bot
 
 async def send_telegram_message(payment: Payments):
     bot = Bot(token=os.getenv('BOT_TOKEN'), parse_mode='HTML')
+    description_type = {'Первоначальная подписка на канал': 'Оплата подписки', 'Ежемесячная подписка на канал': 'Продление подписки'}
+
     try:
-        await bot.send_message(payment.user_id, 'TEST')
+        if payment.status == "succeeded":
+            text = (f'{description_type[payment.description]} на сумму {payment.amount} рублей прошла успешно!\n'
+                    f'Подписка на приватный канал оформлена, вы уже добавлены в канал, перейдите по ссылке.\n\n'
+                    f'Деньги будут списываться ежемесячно автоматически (автоплатёж) для продления подписки, если хотите отменить автоплатёж, нажмите на кнопку "Отменить автоплатёж"')
+            await bot.send_message(payment.user_id, text)
     finally:
         await bot.session.close()
