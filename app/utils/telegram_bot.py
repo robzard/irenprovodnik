d83e0c5cc5d850bc7ee43df71eccbc2c939179e2
robzard <pal-vitek1111@mail.ru>
2024-05-02 17:46:07 +0500

    payment

--- app/utils/telegram_bot.py
+++ app/utils/telegram_bot.py
@@ -5,4 +5,7 @@ from aiogram import Bot
 
 async def send_telegram_message(payment: Payments):
     bot = Bot(token=os.getenv('BOT_TOKEN'), parse_mode='HTML')
-    await bot.send_message(payment.user_id, 'TEST')
+    try:
+        await bot.send_message(payment.user_id, 'TEST')
+    finally:
+        await bot.session.close()
