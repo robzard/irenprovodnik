d246570e5be14a2235e6adde2ea37a6550815d2f
robzard <pal-vitek1111@mail.ru>
2024-05-06 20:29:36 +0500

    celery

---
+++ celery_scheduler/db/requests.py
@@ -0,0 +1,64 @@
+import json
+import os
+from datetime import datetime
+from typing import Tuple, Optional
+
+from aiogram.types import Update
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.future import select
+from sqlalchemy import create_engine, update, and_, func
+
+from sqlalchemy_utils import database_exists, create_database
+
+# from states.states import FsmData
+from .models import User, GrafanaLogs, Base, Payments
+
+db_url = f"postgresql+psycopg://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DATABASE')}?options=-c%20timezone%3DAsia/Yekaterinburg"
+engine = create_engine(db_url, connect_args={"options": "-c timezone=Asia/Yekaterinburg"})
+
+# Создание сессии
+SessionLocal = sessionmaker(bind=engine)
+
+
+# def get_subscriptions_expired():
+#     with SessionLocal() as session:
+#         query = select(Payments).where(
+#             and_(
+#                 Payments.event == 'payment',
+#                 Payments.status == 'succeeded',
+#                 Payments.created_at > func.now()
+#             )
+#         )
+#         result = session.execute(query)
+#         payments = result.scalars().all()
+#         return payments
+
+def get_latest_successful_payments():
+    with SessionLocal() as session:
+        # Подзапрос для выбора максимальной даты created_at для каждого user_id
+        subquery = (
+            select(
+                Payments.user_id,
+                func.max(Payments.created_at).label('max_created_at')
+            ).where(
+                and_(
+                    Payments.event == 'payment',
+                    Payments.status == 'succeeded',
+                    Payments.created_at > func.now()
+                )
+            ).group_by(Payments.user_id)
+        ).subquery()
+
+        # Основной запрос, который присоединяет результаты подзапроса и фильтрует записи по максимальной дате
+        query = (
+            select(Payments)
+            .join(subquery, and_(
+                Payments.user_id == subquery.c.user_id,
+                Payments.created_at == subquery.c.max_created_at
+            ))
+        )
+
+        result = session.execute(query)
+        payments = result.scalars().all()
+        return payments
--- celery_scheduler/tasks.py
+++ celery_scheduler/tasks.py
@@ -4,20 +4,22 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
+from db.requests import get_latest_successful_payments
+
 redis_url = f'{os.getenv("REDIS_HOST")}://{os.getenv("REDIS_HOST")}:{os.getenv("REDIS_PORT")}/{os.getenv("REDIS_DB")}'
 app = Celery('tasks', broker=redis_url, backend=redis_url)
 
 
 @app.task
 def send_notification():
-    # Ваш асинхронный код для отправки уведомления
-    print("Sending notification...")
-    logging.info("Sending notification...")
+    result = get_latest_successful_payments()
+    for el in result:
+        logging.info(el.user_id)
 
 
 app.conf.beat_schedule = {
     'send-notification-every-day': {
         'task': 'tasks.send_notification',
-        'schedule': crontab(hour=20, minute=2),
+        'schedule': crontab(hour=12, minute=31),
     },
 }
