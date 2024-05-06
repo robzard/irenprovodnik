382a6d0d671b283d29ca125cb4401aba10eb126b
robzard <pal-vitek1111@mail.ru>
2024-05-06 21:20:40 +0500

    celery

--- celery_scheduler/tasks.py
+++ celery_scheduler/tasks.py
@@ -20,6 +20,6 @@ def send_notification():
 app.conf.beat_schedule = {
     'send-notification-every-day': {
         'task': 'tasks.send_notification',
-        'schedule': crontab(hour=15, minute=53),
+        'schedule': crontab(hour=16, minute=17),
     },
 }
