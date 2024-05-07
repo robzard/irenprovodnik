9295aebaf9ec42804a20dcffc3666d42e6c681ad
robzard <pal-vitek1111@mail.ru>
2024-05-07 19:28:39 +0500

    celery

--- celery_scheduler/tasks.py
+++ celery_scheduler/tasks.py
@@ -1,20 +1,22 @@
-import logging
-
-from db.requests import get_latest_successful_payments
-
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
-
+import logging
 
 def my_daily_task():
+    print("Выполнение задачи:", datetime.now())
     logging.info("Выполнение задачи:", datetime.now())
-
 
 # Создаем планировщик
 scheduler = BlockingScheduler()
 
-# Добавляем задачу, которая будет выполняться ежедневно в полночь
-scheduler.add_job(my_daily_task, 'cron', hour=14, minute=24)
+# Получаем текущее время
+now = datetime.now()
+minute = now.minute + 1  # Настроим запуск на следующую минуту
+if minute == 60:
+    minute = 0  # Обрабатываем переход через час
+
+# Добавляем задачу, которая будет выполняться в ближайшую минуту
+scheduler.add_job(my_daily_task, 'cron', hour=now.hour, minute=minute)
 
 # Запускаем планировщик
 scheduler.start()
