e2bc59d86ad0fd6db238ff1ca643c252adfb2fb3
robzard <pal-vitek1111@mail.ru>
2024-05-09 17:40:47 +0500

    apscheduler

--- celery_scheduler/db/models.py
+++ celery_scheduler/db/models.py
@@ -23,7 +23,7 @@ class User(Base):
     first_name: Mapped[str | None] = mapped_column(String, nullable=True)
     last_name: Mapped[str | None] = mapped_column(String, nullable=True)
     phone_number: Mapped[str | None] = mapped_column(String, nullable=True)
-    payment_date = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('Asia/Yekaterinburg', now())"))]
+    payment_date: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP"), nullable=True)
     language_code: Mapped[str | None] = mapped_column(String, nullable=True)
     is_admin: Mapped[bool | None] = mapped_column(Boolean, nullable=True)
 
