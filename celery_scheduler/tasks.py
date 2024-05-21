9cb54f0b4a770412e4327ba0f828699c40b9dffc
robzard <pal-vitek1111@mail.ru>
2024-05-21 16:29:15 +0500

    auto payment

--- celery_scheduler/tasks.py
+++ celery_scheduler/tasks.py
@@ -3,14 +3,14 @@ import os
 import sys
 
 # Явное добавление пути к common
-sys.path.insert(0, '/common')
+# sys.path.insert(0, '/common')
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime
 import logging
 import utils.telegram_bot as tg
 
-sys.path.append('/common')
+# sys.path.append('/common')
 
 from common.db.requests import get_users_subscription_expired, set_subscription_false, set_subscription_true
 
